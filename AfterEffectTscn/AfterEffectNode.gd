3132d4c84e756dac30cf1cc5597541ba8d225d92
mequam <blue9ja@gmail.com>
2023-01-21 14:22:18 -0500

    got after effects working on the ghost
    also imported tools from that project

---
+++ AfterEffectTscn/AfterEffectNode.gd
@@ -0,0 +1,75 @@
+extends Node2D
+
+@export var the_sprite : AnimatedSprite2D
+@export var after_image_frequency = 4.0
+@export var after_image_mesh_scene : PackedScene
+@export_range(1,100,1) var after_image_count = 4:
+	set(n_count):
+		after_image_count = n_count
+		set_the_after_image_count(after_image_count)
+
+@export var after_image_slight_delay = 0.0
+func set_the_after_image_count(n_count):
+	for child in get_children():
+		child.queue_free()
+	mesh_array.clear()
+	if n_count <= 0:
+		return
+	mesh_update_index = 0
+	for i in range(n_count):
+		var new_node = after_image_mesh_scene.instantiate()
+		add_child(new_node)
+		mesh_array.append(new_node)
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+var time = 0.0
+var mesh_update_index = 0
+var mesh_array : Array
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.after_image_count = after_image_count
+	# var the_mesh_array = get_children().filter(func(node): return node is MeshInstance2D)
+	# print(the_mesh_array)
+	# print(the_mesh_array.get_typed_class_name())
+	# mesh_array = the_mesh_array
+	pass # Replace with function body.
+
+
+
+
+func _process(delta):
+	time += delta
+	if time >= 1.0/after_image_frequency:
+		update_after_image()
+		time=0.0
+	# time = mod(delta+time,1.0/after_image_frequency)
+	pass
+
+func update_after_image():
+	var the_texture : Texture2D = the_sprite.frames.get_frame_texture(the_sprite.animation,the_sprite.frame)
+	var the_size = the_texture.get_size()
+	var the_mesh_instance = mesh_array[mesh_update_index]
+	var the_mesh = the_mesh_instance.mesh
+	
+	the_mesh.size = the_size
+	the_mesh.size.y *= -1
+	
+	var the_global_transform = the_sprite.global_transform
+
+	mesh_update_index = (1+mesh_update_index)%len(mesh_array)
+
+	the_mesh_instance.material.set_shader_parameter("Start_Time", Time.get_ticks_usec()/1e+6)
+	the_mesh_instance.texture = the_texture
+	the_mesh_instance.global_transform = the_global_transform
+	the_mesh_instance.scale = the_sprite.scale
+	if the_sprite.flip_h:
+		the_mesh_instance.scale.x *= -1
+
+	var the_lifetime = after_image_count * 1.0/after_image_frequency
+	the_mesh_instance.material.set_shader_parameter("Lifetime", the_lifetime)
+	the_mesh_instance.material.set_shader_parameter("SlightDelay", Globals.rendering_start_time)
+
+func _input(event):
+	if event is InputEventKey:
+		if event.keycode == KEY_SPACE:
+			get_tree().paused = !get_tree().paused
--- assets/scripts/singletons/Globals.gd
+++ assets/scripts/singletons/Globals.gd
@@ -6,3 +6,23 @@ extends Node
 #the name of the game save file, stored globaly in a location that the game can
 #reference
 var game_name : String = "game1"
+
+#global time variables used by shaders
+var rendering_start_time = -1
+var first_frame = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	RenderingServer.connect("frame_pre_draw",frame_draw_pre)
+
+
+
+#called right before the first frame of the rendering server
+#used to offset in time between game run time and shader TIME
+func frame_draw_pre():
+	var frame_time_us = Time.get_ticks_usec()
+	var frame_time_s = frame_time_us / 1.0e+6
+	# print(frame_time_s)
+	if first_frame:
+		rendering_start_time = frame_time_s
+		first_frame = false
