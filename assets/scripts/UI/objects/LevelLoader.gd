76eca4877ea0024abeb1856b67db665a35ffa26d
mequam <blue9ja@gmail.com>
2023-05-03 10:51:46 -0400

    adding dither effect + game selction UI

--- assets/scripts/UI/objects/LevelLoader.gd
+++ assets/scripts/UI/objects/LevelLoader.gd
@@ -1,27 +1,40 @@
 extends Control
+#this class represents a UI node that lets the player select a game to do 
+#something to, it could be save delete reset ect.
+
+class_name GameSelector
 
 @export 
-var btnContainer : LeniGarnish
+var btnContainer : LeniGarnish 
+
+#emited when the player selects a given game
+signal game_select
 
 #these are the buttons that we store to start the game!
 var hand_drawn_button = preload("res://scenes/UI/btnHandDrawn.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#append padding at the start
+	#append padding at the start for spacing
 	var padding = Control.new()
 	padding.size_flags_vertical = SIZE_EXPAND 
 	btnContainer.add_child(padding)
 
 	for save in GameSaveResource.list_game_saves():
-		var b = hand_drawn_button.instantiate()
+		var b : BtnHandDrawn = hand_drawn_button.instantiate()
 		b.label.text = save
 		b.size_flags_vertical = SIZE_FILL 
 		b.size_flags_horizontal = SIZE_FILL
 		btnContainer.add_child(b)
-	#append padding at the end
+		b.pressed.connect(on_save_selected.bindv([b]))
+	
+	#append padding at the end for spacing
 	padding = Control.new()
 	padding.size_flags_vertical = SIZE_EXPAND 
 	btnContainer.add_child(padding)
 
+	#register the buttons in the leni garnish once we finish adding them
 	btnContainer.register_buttons()
+
+func on_save_selected(btn):
+	print(btn.label.text)
--- assets/scripts/UI/objects/lblHandDrawn.gd
+++ assets/scripts/UI/objects/lblHandDrawn.gd
@@ -4,3 +4,17 @@ class_name BtnHandDrawn
 
 @export var texture_rect : TextureRect
 @export var label : Label
+
+@export
+var hover_texture  : Texture2D
+@export
+var default_texture : Texture2D
+
+func _ready():
+	self.mouse_entered.connect(on_mouse_entered)
+	self.mouse_exited.connect(on_mouse_exited)
+
+func on_mouse_entered():
+	texture_rect.texture = hover_texture 
+func on_mouse_exited():
+	texture_rect.texture = default_texture 
