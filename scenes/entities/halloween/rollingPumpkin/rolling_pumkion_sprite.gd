e461c03d269f40c4d3c2d06dde4308b7dabfbbb3
mequam <blue9ja@gmail.com>
2023-09-06 14:19:22 -0400

    got squish and ouch animations working :D

--- scenes/entities/halloween/rollingPumpkin/rolling_pumkion_sprite.gd
+++ scenes/entities/halloween/rollingPumpkin/rolling_pumkion_sprite.gd
@@ -1,9 +1,59 @@
-extends Node2D
+extends AnimatedSprite2D
+
+
+@export var animation_player : AnimationPlayer
+@export var rotation_bone : Node2D
+
+@export var eye_rotation_sensativity : float 
+@export var ouch_threshold : float #when we collide harder than this, indicate ouch
+
+var rotation_speed : float :
+	set (val):
+		speed_scale = rotation_speed*eye_rotation_sensativity/2
+		rotation_speed = val 
+		if abs(rotation_speed)  < 0.1:
+			self.rolling = false 
+		elif not self.rolling and abs(rotation_speed) > 0.1:
+			self.custom_play("roll")
+	get:
+		return rotation_speed
+var rolling : bool = false 
+
 
 
 func custom_play(animation : StringName  = "idle",backwards : bool = false)->void:
 	match animation:
 		"roll":
-			$AnimationPlayer.play("roll_prep")
+			if not rolling:
+				stop()
+				animation_player.play("roll_prep")
+			self.rolling = true 
+		"roll_prep_recover":
+			self.stop()
+			animation_player.play(animation)
+		"ouch":
+			stop()
+			animation_player.play("ouch")
 		_:
-			$AnimationPlayer.play(animation)
+			if self.rolling:
+				animation_player.play("RESET") #reset everything
+				animation_player.stop()
+				self.play("eye_roll")
+			else:
+				stop()
+				animation_player.play(animation)
+func _ready()->void:
+	animation_player.animation_finished.connect(self.anim_finished)
+func anim_finished(anim : StringName)->void:
+	print("animation finished on sprite2D " + anim )
+	match anim:
+		"roll_prep":
+			animation_finished.emit("roll_prep")
+			custom_play("roll_prep_recover")
+		"roll_prep_recover":
+			#not really used but just in case we want to use them the strings match the animation that plays
+			custom_play("eye_roll") 
+		"ouch":
+			custom_play("eye_roll")
+func _process(delta)->void:
+	rotation_bone.rotate(delta*rotation_speed)
--- scenes/entities/halloween/rollingPumpkin/rolling_pumpkin_entity.gd
+++ scenes/entities/halloween/rollingPumpkin/rolling_pumpkin_entity.gd
@@ -8,10 +8,12 @@ class_name RotatingPumpkin
 @export var net_force : Vector2 #net force set by the player
 @export var player_control : Vector2
 
-@export var terminal_velocity : float 
+@export var terminal_velocity : Vector2 
 @export var friction : Vector2 #friction proportionality constant 0 is no friction 1 is infinit friction
 
-@export var jump_speed : float
+@export var jump_speed : float 
+
+
 
 func add_impulse(force : Vector2)->void:
 	net_force += force 
@@ -28,12 +30,35 @@ func on_action_released(action : String)->void:
 
 func _physics_process(delta)->void:
 	self.velocity += (net_force + gravity-self.velocity*friction)*delta 
-	if velocity.length() > self.terminal_velocity:
-		self.velocity = self.velocity.normalized()*self.terminal_velocity
+
+	#have x and y terminal velocity seperate to avoid oddities on jump
+	if abs(velocity.x) > self.terminal_velocity.x:
+		self.velocity.x = self.terminal_velocity.x * (-1 if self.velocity.x < 0 else 1)
+	if abs(self.velocity.y) > self.terminal_velocity.y:
+		self.velocity.y = self.terminal_velocity.y * (-1 if self.velocity.y < 0 else 1)
+	
 	var col = self.singal_move_and_collide(self.velocity*delta)
 	if col:	
-		self.velocity -= self.velocity.project(col.get_normal())
+		var normal : Vector2 = col.get_normal()
+		var projection : Vector2 = self.velocity.project(normal)
+		self.velocity -= projection
 		self.singal_move_and_collide(self.velocity*delta) #slide after colliding
+		
+		#updtae the rotation speed and send normal information to the animation system
+		self.get_sprite2D().rotation_speed = self.velocity.length()*(-1 if self.velocity.x < 0 else 1)
+		if self.get_sprite2D().ouch_threshold < projection.length():
+			self.get_sprite2D().custom_play("ouch")
+
+	
+
+func main_ready()->void:
+	self.get_sprite2D().animation_finished.connect(self.anim_finished)
+	super.main_ready()
+
+func anim_finished(anim : StringName)->void:
+	match anim:
+		"roll_prep":
+			self.velocity += Vector2(10 * (-1 if self.velocity.x < 0 else 1),-20) #quick boost to velocity after the pumpkin "thrusts"
 
 func main_process(delta)->void:
 	super.main_process(delta)
