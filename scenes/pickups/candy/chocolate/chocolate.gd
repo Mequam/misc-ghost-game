4c9b4043e2b246c507533a16f2aee71cc51505cc
mequam <blue9ja@gmail.com>
2023-10-23 13:00:38 -0400

    finished basic candy pickup code

---
+++ scenes/pickups/candy/chocolate/chocolate.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+class_name Candy
+
+@export var heal_amount : int = 2
+@export var animation_player : AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_player.play("default")	
+	self.body_entered.connect(on_body_entered)
+
+func on_body_entered(bod)->void:
+	if bod is Leni:
+		bod.health += self.heal_amount 
+		queue_free()
