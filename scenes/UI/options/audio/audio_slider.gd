0c127719ebc184f3d61ec88f08fa5aa3b4636b2d
mequam <blue9ja@gmail.com>
2023-12-08 20:11:31 -0500

    got test noises working and fixed sum boogs
    TODO: double check mixing on the test noise

--- scenes/UI/options/audio/audio_slider.gd
+++ scenes/UI/options/audio/audio_slider.gd
@@ -7,6 +7,8 @@ extends Control
 
 @export var sliderLenis : Array[Texture2D]
 
+@export var audio_overkill_perc : float = 2
+
 
 var audio_settings : GlobalGameSettings
 
@@ -15,9 +17,11 @@ var audio_bus : int = 0
 
 #sets the audio bus that we will change and control in the game
 func set_bus(bus_number : int )->void:
-	lblName.set_text(AudioServer.get_bus_name(bus_number))
+	var bus_name : StringName = AudioServer.get_bus_name(bus_number) 
+	lblName.set_text(bus_name)
 	audio_bus = bus_number
-	audioSlider.value = 100*db_to_linear(AudioServer.get_bus_volume_db(bus_number))
+	audioSlider.value = audioSlider.max_value*db_to_linear(AudioServer.get_bus_volume_db(bus_number))/self.audio_overkill_perc
+	$AudioStreamPlayer.set_bus(bus_name)
 	self.sync_display()
 
 func _ready()->void:
@@ -30,12 +34,13 @@ func on_drag_started()->void:
 
 #returns the volumne of our audio bus as a linear 
 func get_linear_vol()->float:
-	return 100*db_to_linear(AudioServer.get_bus_volume_db(audio_bus)) 
+	return audioSlider.max_value*db_to_linear(AudioServer.get_bus_volume_db(audio_bus))/self.audio_overkill_perc 
 
 func _on_h_slider_drag_ended(changed : bool)->void:
 	#save the settings back out to the disk
 	audio_settings.save_settings()
 	audio_settings = null
+	$AudioStreamPlayer.play()
 
 func get_appropriate_slider_leni()->Texture2D:
 	var x = int((len(sliderLenis)-1)*(audioSlider.value+10)/100)
@@ -45,11 +50,12 @@ func get_appropriate_slider_leni()->Texture2D:
 func sync_display()->void:
 	audioSlider.add_theme_icon_override("grabber",get_appropriate_slider_leni())
 	audioSlider.add_theme_icon_override("grabber_highlight",get_appropriate_slider_leni())
-	self.lblPerc.text = "%d"%(2*self.get_linear_vol())+"%"
+	self.lblPerc.text = "%d"%(self.audio_overkill_perc*self.get_linear_vol())+"%"
 	lblPerc.add_theme_color_override("background_color",Color.WHITE if audioSlider.value < 49 else Color.BLACK)
 
 
 func _on_hslider_audio_value_changed(value : float)->void:
 	if audio_settings:
-		audio_settings.set_audio_vol_from_linear(self.audio_bus,self.audioSlider.value/100)
+		audio_settings.set_audio_vol_from_linear(self.audio_bus,
+			self.audio_overkill_perc*self.audioSlider.value/self.audioSlider.max_value)
 	self.sync_display()
