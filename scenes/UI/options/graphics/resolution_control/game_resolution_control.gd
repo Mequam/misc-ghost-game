d630ff21f96c4315c8907dfb7b3b3f5d81a55723
mequam <blue9ja@gmail.com>
2023-12-30 19:45:10 -0500

    finished aspect ratio ui!

--- scenes/UI/options/graphics/resolution_control/game_resolution_control.gd
+++ scenes/UI/options/graphics/resolution_control/game_resolution_control.gd
@@ -14,27 +14,49 @@ func sync_spin_boxes()->void:
 	width_control.value = size.x
 	height_control.value = size.y
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.sync_spin_boxes()
-
-	var size : Vector2i = get_tree().get_root().content_scale_size
+func sync_present_selection()->void:
+	var aspect_size : Vector2i = get_tree().get_root().content_scale_size
 	
 	#ensure that if we are using a present we indicate that to the user
 	for i in range(resolution_presents.item_count):
-		if get_resolution_present(i) == size:
+		if get_resolution_present(i) == aspect_size:
 			resolution_presents.select(i)
+			return
+	#if nothing is selected, ensure that we indicate the present is not selected
+	resolution_presents.deselect_all()
+
+func sync_display()->void:
+	self.sync_spin_boxes()
+	self.sync_present_selection()
+
+	#ensure that the proper save button is visible
+	self.update_save_visibility()
+	
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.sync_display()
+
 
 	width_control.value_changed.connect(self.on_width_control_changed)
 	height_control.value_changed.connect(self.on_height_control_changed)
 
 	btn_save.pressed.connect(on_btn_save_pressed)
 	btn_restore.pressed.connect(on_btn_restore_pressed)
+	btn_reset.pressed.connect(on_btn_reset_pressed)
 	resolution_presents.item_selected.connect(self.on_present_selected)
+
+
 
 	self.sync_spin_boxes()
 
+func on_btn_reset_pressed()->void:
+	var settings : GlobalGameSettings = GlobalGameSettings.load_settings()
+	settings.set_screen_resolution(Vector2i(1152,648),get_tree())
+	settings.save_settings()
+
+	self.sync_display()
 
 func get_resolution_present(idx : int)->Vector2i:
 	if resolution_presents.item_count <= idx: return Vector2i(0,0)
@@ -48,13 +70,12 @@ func on_present_selected(idx : int)->void:
 	settings.set_screen_resolution(present,get_tree())
 	settings.save_settings()
 
-	self.sync_spin_boxes()
+	self.sync_display()
+
+
 
 func on_btn_restore_pressed()->void:
-	self.sync_spin_boxes()
-	
-	#ensure that we can see (or not see) the save button
-	update_save_visibility()
+	self.sync_display()
 
 func on_btn_save_pressed()->void:
 	
@@ -62,8 +83,8 @@ func on_btn_save_pressed()->void:
 	var settings : GlobalGameSettings = GlobalGameSettings.load_settings()
 	settings.set_screen_resolution(Vector2i(int(width_control.value),int(height_control.value)),get_tree())
 	settings.save_settings()
-
-	update_save_visibility()
+	
+	self.sync_display()
 
 func update_save_visibility()->void:
 	var resolution : Vector2i = get_tree().get_root().content_scale_size
