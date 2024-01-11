a8879470d5ccc15e1918cf6f53f67007f023d5a7
mequam <blue9ja@gmail.com>
2024-01-11 15:27:32 -0500

    added buffer on the interaction for npc animations

--- scenes/entities/tools/AnimationZoneTrigger.gd
+++ scenes/entities/tools/AnimationZoneTrigger.gd
@@ -17,8 +17,8 @@ class_name AnimationZoneTrigger
 #if set the animation that the entity will default to playing
 @export var ready_animation : String = ""
 
-
-
+var contains_player : bool = false
+var contained_player : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,12 +29,31 @@ func _ready():
 	self.body_exited.connect(self.on_entity_exited)
 	if ready_animation != "": get_parent().get_sprite2D().play(self.ready_animation)
 
-func on_entity_entered(_body : Node2D):
-	if do_play_enter:
+	#the buffer before we play the animations, used to limit
+	#animation flickering
+	$interaction_timer.timeout.connect(self.interaction_timed_out)
+
+func interaction_timed_out()->void:
+	#detect the rising edge of the player entering
+	if do_play_enter and contains_player and not contained_player:
 		get_parent().get_sprite2D().custom_play(self.play_on_enter)
+	if do_play_exit and not contains_player and contained_player:
+		get_parent().get_sprite2D().custom_play(self.play_on_exit)
+	contained_player = contains_player
+
+
+#says we want to start an interaction routine,
+#we only do this if we are not already interacting
+func trigger_interaction()->void:
+	if $interaction_timer.time_left == 0:
+		$interaction_timer.start()
+
+func on_entity_entered(_body : Node2D):
+	contains_player = true
+	self.trigger_interaction()
 
 func on_entity_exited(_body : Node2D):
-	if do_play_exit:
-		get_parent().get_sprite2D().custom_play(self.play_on_exit)
+	contains_player = false
+	self.trigger_interaction()
 
 
