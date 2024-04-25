32dda7669ab3d5e8218e46941d8f610188b5916c
mequam <blue9ja@gmail.com>
2024-04-25 12:24:30 -0400

    rotating can lamp WIP

--- scenes/effects/AfterEffectTscn/AfterEffectNode.gd
+++ scenes/effects/AfterEffectTscn/AfterEffectNode.gd
@@ -94,7 +94,6 @@ func update_after_image():
 		mesh_update_index = (1+mesh_update_index)%len(mesh_array)
 
 		the_mesh_instance.material.set_shader_parameter("Start_Time", Time.get_ticks_usec()/1e+6)
-		print_debug("setting a color!")
 		the_mesh_instance.material.set_shader_parameter("ColorA0", color_a)
 		the_mesh_instance.material.set_shader_parameter("ColorB", color_b)
 
---
+++ scenes/levels/orchard/entryLevel/weak_lamp.gd
@@ -0,0 +1,21 @@
+extends RespawnLamp
+
+var forward : bool = true
+
+func _ready()->void:
+	super._ready()
+	var s = get_sprite2D()
+	s.play("default")
+	s.frame_changed.connect(on_frame_changed)
+	s.animation_looped.connect(on_anim_looped)
+
+
+
+func on_anim_looped()->void:
+	forward = not forward
+
+func on_frame_changed()->void:
+	#if forward:
+	$pivot.rotation_degrees = remap(float(get_sprite2D().frame),2,3,5,-5)
+	#else:
+	#	$pivot.rotation_degrees = remap(float(get_sprite2D().frame),2,3,-5,5)
