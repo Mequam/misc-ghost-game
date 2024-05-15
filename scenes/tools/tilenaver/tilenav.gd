be4a8199c02b4133e04ea8663bce62ed02073e77
mequam <blue9ja@gmail.com>
2024-05-15 15:00:59 -0400

    flight entity tile map baking!

--- scenes/tools/tilenaver/tilenav.gd
+++ scenes/tools/tilenaver/tilenav.gd
@@ -14,23 +14,17 @@ class_name TileNav
 	set (val):
 		if val != null:
 			var tmap : TileMap = val as TileMap
-			var bounding_box = tmap.get_used_rect()
 			tmap.add_layer(-1)
 			tmap.set_layer_name(-1,"navigation tool output")
 			
-			for x in bounding_box.size.x:
-				for y in bounding_box.size.y:
-					tmap.set_cell(
-						-1,
-						Vector2i(x,y)+bounding_box.position,
-						navigation_tile_id,
-						navigation_atlas_chord
-						)
 			for point : Vector2i in tmap.get_used_cells_by_id(target_layer):
 				for x in range(x_offset):
 					for y in range(jump_height):
-						tmap.set_cell(-1,point+Vector2i(x,-y)) #clear out the cells from the tile map
-						tmap.set_cell(-1,point+Vector2i(-x,-y)) #clear out the cells from the tile map
+						tmap.set_cell(-1,point+Vector2i(x,-y),navigation_tile_id,navigation_atlas_chord)
+						tmap.set_cell(-1,point+Vector2i(-x,-y),navigation_tile_id,navigation_atlas_chord)
+
+			for point : Vector2i in tmap.get_used_cells_by_id(target_layer):
+				tmap.set_cell(-1,point)
 	get:
 		return tile_map_reference
 
