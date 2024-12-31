88d8b7cbdfa941a540c94af9d024d4ebbab8b369
mequam <blue9ja@gmail.com>
2024-12-31 15:23:49 -0500

    local to scene shader

--- scenes/entities/misc/groundHog/ground_hog_enemy.gd
+++ scenes/entities/misc/groundHog/ground_hog_enemy.gd
@@ -6,10 +6,12 @@ extends Entity
 class_name GroundHogEnemy
 
 @export var growth_speed : float = 0.01
+#the highest we can grow too
 @export var max_growth : float = 100.0
-
-#collision shape referenec we change with our height
-@export var collision_shape : CollisionShape2D
+#the lowest we can grow to
+@export var min_growth : float = 10.0
+#if true we set max growth from the current scale of the entity on ready
+@export var set_max_growth : bool = true
 
 @export var sprite_bone : Node2D
 var height : float :
@@ -18,6 +20,11 @@ var height : float :
 		self.scale.y = val
 	get:
 		return self.scale.y
+
+func _ready() -> void:
+	if self.set_max_growth:
+		self.max_growth = self.height
+
 
 func _process(delta: float) -> void:
 	if self.pressed_inputs["UP"] and self.max_growth >= self.height:
